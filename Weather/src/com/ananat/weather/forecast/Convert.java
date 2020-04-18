1ee1e38bba2ab04555211bd462b039d89fd1da87
Anant jain <jainanant990@gmail.com>
2020-04-18 15:45:49 +0530

    first commit from eclipse

---
+++ Weather/src/com/ananat/weather/forecast/Convert.java
@@ -0,0 +1,46 @@
+package com.ananat.weather.forecast;
+
+public class Convert {
+	
+	String name;
+	int rollno;
+	static String cllgname;
+	static int count = 0;
+	
+	static{
+		cllgname = "BKBIET";
+	}
+	
+	public Convert(String str)
+	{
+		this.name = str;
+		this.rollno = rolno();
+	}
+	 static int rolno()
+	{
+		count++;
+		return count;
+	}
+	void printinfo()
+	{
+		System.out.println(name+" "+rollno+" "+cllgname);
+	}
+	
+	public static void main(String[] args) {
+		
+		Convert c1 = new Convert("Anant jain");
+		Convert c2 = new Convert("sujal jain");
+		Convert c3 = new Convert("shreyansh jain");
+		
+		c1.printinfo();
+		c2.printinfo();
+		c3.printinfo();
+		
+		
+		
+		
+		
+		
+		
+	}
+}
